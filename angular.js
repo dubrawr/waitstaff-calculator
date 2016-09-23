a591fc52ce7f36ca3b24cdb53cd7d371f85a4630
Deb <deborah.tsao@gmail.com>
2016-09-22 21:44:45 -0700

    everything works now!

--- angular.js
+++ angular.js
@@ -1,20 +1,44 @@
 angular.module('myApp', ['ngMessages'])
 .controller('Controller', function($scope){
 	var vm = this;
-	$scope.basemealprice = 0;
-	$scope.tippercentage = 0;
-	$scope.taxrate = 0;
-	vm.tiprate = function(){
-		return $scope.basemealprice * ($scope.tippercentage/100);
-		};
-	vm.taxrate = function(){
-		return $scope.basemealprice * ($scope.taxrate/100);
+	$scope.allTip = 0;		
+	$scope.averageTip = 0;
+
+	var resetFields = function(){
+	$scope.basemealprice = '';
+	$scope.tippercentage = '';
+	$scope.taxrate = '';
 	};
-	vm.subtotal = function(){
-		return $scope.basemealprice + vm.taxrate();
-	};
-	vm.total = function(){
-		return vm.subtotal() + vm.tiprate();
+
+	$scope.count = 0;
+	$scope.hideRight = false;
+	$scope.showError = false;
+	$scope.submit = function(){
+		if ($scope.leftForm.$invalid){
+			$scope.showError = true;
+			$scope.formError = 'Please fill in valid inputs.';
+		} else {
+		$scope.showError = false;
+		$scope.hideRight = true;
+		$scope.count++;
+		$scope.tiptotal = $scope.basemealprice * ($scope.tippercentage/100);
+		$scope.taxtotal = $scope.basemealprice * ($scope.taxrate/100);
+		$scope.subtotal = $scope.basemealprice + $scope.taxtotal;
+		$scope.total = $scope.subtotal + $scope.tiptotal;
+		$scope.allTip += $scope.tiptotal;
+		$scope.averageTip = ($scope.allTip/$scope.count);
+		resetFields();
+
+	}
+};
+	$scope.resetEntire = function(){
+		resetFields();
+		$scope.allTip = '';
+		$scope.averageTip = '';
+		$scope.count = 0;
+		$scope.subtotal = '';
+		$scope.tiptotal = '';
+		$scope.total = '';
 	};
 });
 
